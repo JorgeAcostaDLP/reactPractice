afd01749500f054c1f3c51215252a734592cdbb8
JorgeAcostaDLP <jh.acostadelapena@gmail.com>
2019-10-11 15:31:38 -0400

    Number 7 game with conditionals

--- JSXDemo.js
+++ JSXDemo.js
@@ -1,15 +1,43 @@
-const myMood = () => {
-  const moods = ['angry', 'happy', 'sad', 'desperate'];
-  return moods[Math.floor(Math.random() * moods.length)];
+// My mood
+// const myMood = () => {
+//   const moods = ['angry', 'happy', 'sad', 'desperate'];
+//   return moods[Math.floor(Math.random() * moods.length)];
+// };
+// class JSXDemo extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>My mood is: {myMood()}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+//Lucky 7
+const getNum = () => {
+  return Math.floor(Math.random() * 10 + 1);
 };
-class JSXDemo extends React.Component {
+class NumPicker extends React.Component {
   render() {
+    let number = getNum();
+    let msg;
+    if (number === 7) {
+      msg = (
+        <div>
+          <h1>YOU WIN!!</h1>
+          <img src='https://media1.giphy.com/media/3o7TKPXoWOJO1Oa3Go/giphy.gif' />
+        </div>
+      );
+    } else {
+      msg = <p>Sorry, you are a loser but definitely try again.</p>;
+    }
+
     return (
-      <div>
-        <h1>My mood is: {myMood()}</h1>
-      </div>
+      <h1>
+        Your Number is: {number}, {msg}
+      </h1>
     );
   }
 }
 
-ReactDOM.render(<JSXDemo />, document.getElementById('root'));
+ReactDOM.render(<NumPicker />, document.getElementById('root'));
