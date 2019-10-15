8b9bbb3848469930f6e48fd5e4cb12a9372590bd
JorgeAcostaDLP <jh.acostadelapena@gmail.com>
2019-10-15 13:51:00 -0400

    Looping in JSX

--- Hello.js
+++ Hello.js
@@ -2,10 +2,18 @@ class Hello extends React.Component {
   render() {
     console.log(this.props);
     let props = this.props;
+    let bangs = '!'.repeat(props.bangs);
     return (
       <div>
         <h1>
           Hello {props.name1} from {props.name2}
+          <ul>
+            My hobbies are :
+            {props.hobbies.map(hob => (
+              <li>{hob}</li>
+            ))}
+          </ul>
+          {bangs}
         </h1>
       </div>
     );
--- index.js
+++ index.js
@@ -2,7 +2,20 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Hello name1='Jorge' name2='Luis' />;
+        <Hello
+          name1='Jorge'
+          name2='Luis'
+          bangs={4}
+          hobbies={['laughing', 'fun']}
+        />
+        ;
+        <Hello
+          name1='Jorge'
+          name2='Luis'
+          bangs={40}
+          hobbies={['drinking', 'fugogon']}
+        />
+        ;
         <NumPicker />
       </div>
     );
